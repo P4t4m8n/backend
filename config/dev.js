4228578647caa8b7ca2cb13bb7e2a34d5d1ab973
P4t4m8n <eranopenuni@gmail.com>
2024-01-17 02:34:36 +0200

    asasa

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 export default {
-    // dbURL: 'mongodb://127.0.0.1:27017',
-    dbURL: 'mongodb+srv://P4t4m8n:DTjNYB1ALFsgqVT6@tubfy.vzq1ydk.mongodb.net/?retryWrites=true&w=majority',
-    dbName: 'Tubefy',
+    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb+srv://P4t4m8n:DTjNYB1ALFsgqVT6@tubfy.vzq1ydk.mongodb.net/?retryWrites=true&w=majority',
+    dbName: 'youtubefy_db',
 
 }
